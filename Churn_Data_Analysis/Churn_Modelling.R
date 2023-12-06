aa4d38b3d4be4172cbf3cdd85340e57c2dd35923
Masun <46847469+MesunNeciyev@users.noreply.github.com>
2023-12-06 10:39:15 +0400

    Add files via upload

---
+++ Churn_Data_Analysis/Churn_Modelling.R
@@ -0,0 +1,201 @@
+#implementing libraries and installation  packages
+library(tidyverse)
+library(skimr)
+library(inspectdf)
+library(caret)
+library(glue)
+library(highcharter)
+library(h2o)
+library(scorecard)
+library(yardstick)
+library(ggsci)
+
+
+
+df<-read_csv("Churn_Modelling.csv")
+
+df %>% view()
+
+glimpse(df)
+
+df %>% inspect_na()
+
+df$Exited<-df$Exited %>% as.factor()
+
+lapply(df, class)
+
+
+prop.table(table(df$Exited))
+
+skimr::skim(df)
+
+
+# Exclude unimportant variables by IV(>0.02)------------
+
+ivars<-df %>% 
+  iv(y='Exited') %>% 
+  as_tibble() %>% 
+  mutate(info_value=round(info_value,3)) %>% 
+  arrange(desc(info_value))
+
+ivars<-ivars %>% filter(info_value>0.02)
+
+ivars<-ivars[[1]]
+
+df<-df %>% select(Exited,ivars)
+
+df<-df %>% select(-Surname)
+
+df %>% view()
+
+#Split data into train and test sets using seed=123---------
+dt_list<-df %>% split_df('Exited',ratio=0.8,seed=123)
+
+#Applying binning according to weight of evidence principle--------
+
+df %>% filter(Geography=='France') %>% count()
+
+df %>% woebin('Exited')->bins
+bins$Balance %>% woebin_plot()
+bins$Geography %>% as_tibble() %>% view()
+bins$Balance %>% woebin_plot()
+
+
+train_woe<-dt_list$train %>% woebin_ply(bins)
+test_woe<-dt_list$test %>% woebin_ply(bins)
+
+test_woe %>% view()
+
+names<-names(train_woe)
+names<-gsub('_woe','',names)
+
+names(train_woe)<-names
+names(test_woe)<-names
+
+
+# Multicollinearity ----
+
+# coef_na
+target <- 'Exited'
+features <- train_woe %>% select(-Exited) %>% names()
+
+f <- as.formula(paste(target, paste(features, collapse = " + "), sep = " ~ "))
+glm <- glm(f, data = train_woe, family = "binomial")
+glm %>% summary()
+
+#alias for linearly corr
+coef_na <- attributes(alias(glm)$Complete)$dimnames[[1]]
+features <- features[!features %in% coef_na]
+f <- as.formula(paste(target, paste(features, collapse = " + "), sep = " ~ "))
+glm <- glm(f, data = train_woe, family = "binomial")
+
+
+# VIF (Variance Inflation Factor) 
+while(glm %>% vif() %>% arrange(desc(gvif)) %>% .[1,2] >= 3){
+  afterVIF <- glm %>% vif() %>% arrange(desc(gvif)) %>% .[-1,"variable"]
+  afterVIF<-afterVIF$variable
+  f <- as.formula(paste(target, paste(afterVIF, collapse = " + "), sep = " ~ "))
+  glm <- glm(f, data = train_woe, family = "binomial")
+}
+
+glm %>% vif() %>% arrange(desc(gvif)) %>% pull(variable) -> features 
+
+
+# VIF (Variance Inflation Factor) 
+f <- as.formula(paste(target, paste(features, collapse = " + "), sep = " ~ "))
+glm <- glm(f, data = train_woe, family = "binomial")
+
+while(glm %>% vif() %>% arrange(desc(gvif)) %>% .[1,2] >= 1.5){
+  afterVIF <- glm %>% vif() %>% arrange(desc(gvif)) %>% .[-1,"variable"]
+  f <- as.formula(paste(target, paste(afterVIF, collapse = " + "), sep = " ~ "))
+  glm <- glm(f, data = train_woe, family = "binomial")
+}
+
+glm %>% vif() %>% arrange(desc(gvif)) %>% pull(variable) -> features 
+
+
+# Modeling with GLM ----
+h2o.init()
+
+train_h2o <- train_woe %>% select(target,features) %>% as.h2o()
+test_h2o <- test_woe %>% select(target,features) %>% as.h2o()
+
+model <- h2o.glm(
+  x = features, y = target, family = "binomial", 
+  training_frame = train_h2o, validation_frame = test_h2o,
+  nfolds = 10, seed = 123, remove_collinear_columns = T,
+  balance_classes = T, lambda = 0, compute_p_values = T)
+
+while(model@model$coefficients_table %>%
+      as.data.frame() %>%
+      select(names,p_value) %>%
+      mutate(p_value = round(p_value,3)) %>%
+      .[-1,] %>%
+      arrange(desc(p_value)) %>%
+      .[1,2] >= 0.05){
+  model@model$coefficients_table %>%
+    as.data.frame() %>%
+    select(names,p_value) %>%
+    mutate(p_value = round(p_value,3)) %>%
+    filter(!is.nan(p_value)) %>%
+    .[-1,] %>%
+    arrange(desc(p_value)) %>%
+    .[1,1] -> v
+  features <- features[features!=v]
+  
+  train_h2o <- train_woe %>% select(target,features) %>% as.h2o()
+  test_h2o <- test_woe %>% select(target,features) %>% as.h2o()
+  
+  model <- h2o.glm(
+    x = features, y = target, family = "binomial", 
+    training_frame = train_h2o, validation_frame = test_h2o,
+    nfolds = 10, seed = 123, remove_collinear_columns = T,
+    balance_classes = T, lambda = 0, compute_p_values = T)
+}
+model@model$coefficients_table %>%
+  as.data.frame() %>%
+  select(names,p_value) %>%
+  mutate(p_value = round(p_value,3))
+
+model@model$coefficients %>%
+  as.data.frame() %>%
+  mutate(names = rownames(model@model$coefficients %>% as.data.frame())) %>%
+  `colnames<-`(c('coefficients','names')) %>%
+  select(names,coefficients)
+
+h2o.varimp(model) %>% as.data.frame() %>% .[.$percentage != 0,] %>%
+  select(variable, percentage) %>%
+  hchart("pie", hcaes(x = variable, y = percentage)) %>%
+  hc_colors(colors = 'orange') %>%
+  hc_xAxis(visible=T) %>%
+  hc_yAxis(visible=T)
+
+# ---------------------------- Evaluation Metrices ----------------------------
+
+# Prediction & Confision Matrice
+pred <- model %>% h2o.predict(newdata = test_h2o) %>% 
+  as.data.frame() %>% select(p1,predict) %>% view()
+
+model %>% h2o.performance(newdata = test_h2o) %>%
+  h2o.find_threshold_by_max_metric('f1')
+
+eva <- perf_eva(
+  pred = pred %>% pull(p1),
+  label = dt_list$test$Exited %>% as.character() %>% as.numeric(),
+  binomial_metric = c("auc","gini"),
+  show_plot = "roc")
+
+
+eva$confusion_matrix$dat
+
+# Check overfitting ----
+model %>%
+  h2o.auc(train = T,
+          valid = T,
+          xval = T) %>%
+  as_tibble() %>%
+  round(2) %>%
+  mutate(data = c('train','test','cross_val')) %>%
+  mutate(gini = 2*value-1) %>%
+  select(data,auc=value,gini)
+
